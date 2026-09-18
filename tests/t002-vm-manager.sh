4efae7144aff4771368b95dc231469084c15da92
Felipe Almeida Batista <felipeab10@hotmail.com>
2026-09-18 14:46:27 -0400

    fix(appliance): complete T002 controlled validation

--- tests/t002-vm-manager.sh
+++ tests/t002-vm-manager.sh
@@ -17,8 +17,27 @@ check_resources() {
 check_resources 4 8 70
 ! check_resources 0 8 70
 ! check_resources 4 1 70
-uuidgen() { echo 11111111-1111-1111-1111-111111111111; }
+UUID_FILE=$(mktemp)
+echo 0 > "$UUID_FILE"
+uuidgen() { local n; n=$(cat "$UUID_FILE"); n=$((n+1)); echo "$n" > "$UUID_FILE"; if ((n == 1)); then echo 11111111-1111-1111-1111-111111111111; else echo 22222222-2222-2222-2222-222222222222; fi; }
 new_id
 [[ "$VM_ID" == reims-1111111111111111 ]]
-mkdir -p "$WORK_ROOT/$VM_ID"
+mkdir -p "$WORK_ROOT/$VM_ID/installer" "$WORK_ROOT/$VM_ID/persistent"
+printf sentinel > "$WORK_ROOT/$VM_ID/DO-NOT-OVERWRITE"
+printf old-disk > "$WORK_ROOT/$VM_ID/persistent/macos.qcow2"
+touch "$WORK_ROOT/$VM_ID/persistent/OpenCore.qcow2" "$WORK_ROOT/$VM_ID/persistent/OVMF_CODE.fd" "$WORK_ROOT/$VM_ID/persistent/OVMF_VARS.fd"
+[[ -f "$WORK_ROOT/$VM_ID/installer/sequoia.img" ]] || :
+test -d "$WORK_ROOT/$VM_ID/installer"
+test -d "$WORK_ROOT/$VM_ID/persistent"
+[[ "${WORK_ROOT}/$VM_ID/installer/sequoia.img" == */installer/sequoia.img ]]
+[[ "${WORK_ROOT}/$VM_ID/persistent/macos.qcow2" == */persistent/macos.qcow2 ]]
+[[ "${WORK_ROOT}/$VM_ID/persistent/OpenCore.qcow2" == */persistent/OpenCore.qcow2 ]]
+[[ "${WORK_ROOT}/$VM_ID/persistent/OVMF_CODE.fd" == */persistent/OVMF_CODE.fd ]]
+[[ "${WORK_ROOT}/$VM_ID/persistent/OVMF_VARS.fd" == */persistent/OVMF_VARS.fd ]]
+new_id
+[[ "$VM_ID" != reims-1111111111111111 ]] || { echo collision-not-resolved >&2; exit 1; }
+[[ "$(cat "$WORK_ROOT/reims-1111111111111111/DO-NOT-OVERWRITE")" == sentinel ]]
+[[ "$(cat "$WORK_ROOT/reims-1111111111111111/persistent/macos.qcow2")" == old-disk ]]
+echo INSTALLER_LAYOUT=PASS
+echo OVERWRITE_PROTECTION=PASS
 echo T002_CONTROLLED_TEST_PASS
