bcda6d726be6a7a400155babdef2789f3e9f99b4
Felipe Almeida Batista <felipeab10@hotmail.com>
2026-09-22 16:00:30 -0400

    probe guest Metal GPU families

--- scripts/browser-probe/guest_opengl_caps.js
+++ scripts/browser-probe/guest_opengl_caps.js
@@ -13,10 +13,39 @@ if (!device) {
 } else {
   const selector = $.NSSelectorFromString("supportsOpenGL");
   const available = Boolean(device.respondsToSelector(selector));
+  const familySelector = $.NSSelectorFromString("supportsFamily:");
+  const familySelectorAvailable = Boolean(
+    device.respondsToSelector(familySelector),
+  );
+  const families = {
+    Apple1: 1001,
+    Apple2: 1002,
+    Apple3: 1003,
+    Apple4: 1004,
+    Apple5: 1005,
+    Apple6: 1006,
+    Apple7: 1007,
+    Apple8: 1008,
+    Mac1: 2001,
+    Mac2: 2002,
+    Common1: 3001,
+    Common2: 3002,
+    Common3: 3003,
+    MacCatalyst1: 4001,
+    MacCatalyst2: 4002,
+  };
+  const supportedFamilies = {};
+  if (familySelectorAvailable) {
+    Object.keys(families).forEach((name) => {
+      supportedFamilies[name] = Boolean(device.supportsFamily(families[name]));
+    });
+  }
   const result = {
     device: ObjC.unwrap(device.name),
     supportsOpenGLSelectorAvailable: available,
     supportsOpenGL: available ? Boolean(device.supportsOpenGL) : null,
+    supportsFamilySelectorAvailable: familySelectorAvailable,
+    supportedFamilies: familySelectorAvailable ? supportedFamilies : null,
   };
   JSON.stringify(result);
 }
